37875f752a36d62d06f42f9aad174a5de7a81128
interactivepix <122866945+interactivepix@users.noreply.github.com>
2023-07-13 07:18:15 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const text= "This is an auto displayed text that can be very handy";
+
+let index = 0;
+
+function writeText() {
+    document.body.innerText = text.slice(0, index);
+
+    index++;
+
+    if (index > text.length) {
+        index = 0;
+    }
+}
+
+setInterval(writeText, 150);
